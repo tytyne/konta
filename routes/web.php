9b19917e16cddbc012d258106d965cdeafec699a
tytyne <njoanc@gmail.com>
2019-11-11 16:26:50 +0200

    frontend part

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth/login');
+});
+
+Auth::routes(['verify'=>true]);
+
+Route::get('profile',function(){
+    return '<h1> This is profile page</h1>';
+})->middleware('verified');
+
+Route::get('/home', 'HomeController@index')->name('home');
